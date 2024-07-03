8fd1ee916faa0b59421c9f436226f314d453efad
pny-mern <pnyflutter@gmail.com>
2024-07-03 16:55:26 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>This is Testing App for Deployment</h1>
+     <button>Register</button>
+    </div>
+  );
+}
+
+export default App;
